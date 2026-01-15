8cd4aab446f4172163e34d2124ea5b83ab1bd690
Micheline Doughlin <86533967+MDoughlin@users.noreply.github.com>
2026-01-15 17:55:53 -0500

    Add solution - CodeHub

---
+++ 7-kyu/convert-the-score/convert-the-score.js
@@ -0,0 +1,24 @@
+function scoreboard(string) {
+  //input = string
+  //output = array of 2 numbers 
+  
+ let score = string.split(" ").slice(-2)
+ 
+return score.map(word => {
+  switch(word){
+ case "one": return 1
+ case "two": return 2
+ case "three": return 3
+ case "four": return 4
+ case "five": return 5
+ case "six": return 6
+ case "seven": return 7
+ case "eight": return 8
+ case "nine":  return 9
+ case "nil": return 0
+ }    
+  })
+}
+​
+​
+​
