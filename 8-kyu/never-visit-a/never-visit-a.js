e9c67fdbc351ea45e7d79d93d7743b2531e27464
Micheline Doughlin <86533967+MDoughlin@users.noreply.github.com>
2026-02-02 19:19:18 -0500

    Add solution - CodeHub

---
+++ 8-kyu/never-visit-a/never-visit-a.js
@@ -0,0 +1,34 @@
+85:'cherry',
+86:'grape',
+87:'cherry',
+88:'pear',
+89:'cherry',
+90:'apple',
+91:'kiwi',
+92:'banana',
+93:'kiwi',
+94:'banana',
+95:'melon',
+96:'banana',
+97:'melon',
+98:'pineapple',
+99:'apple',
+100:'pineapple'
+};
+​
+function subtractSum(n){
+return "apple"
+​
+​
+// let digits = String(n).split("").map(Number)
+// let sum = digits.reduce((a, c) => a + c, 0)
+​
+// let difference = n - sum
+​
+​
+// if (fruits[difference]){
+//   return fruits[difference]
+// } else {
+//   return 1
+// }
+}
